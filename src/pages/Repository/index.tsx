decebd407eef7cbc53039683742fc9e3d094e026
Samuel Santana <samuel.ssa89@gmail.com>
2020-06-06 20:33:41 -0300

    add repository info

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,41 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="algumaccoisa" alt="rocketseat" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+    </>
+  );
 };
 
 export default Repository;
